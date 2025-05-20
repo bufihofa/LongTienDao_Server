e61272ed696f67ceafb0b3d355f6ce35adb2f96c
bufi.hofa <hoapri123@gmail.com>
2025-05-20 14:45:57 +0700

    update dungeon

--- src/common/config/dungeon/dungeon.ts
+++ src/common/config/dungeon/dungeon.ts
@@ -1,2 +1,25 @@
 export enum DungeonType {
+    RungUMinh = 1001, // Rừng U Minh
+    RungCucPhuong = 1002, // Rừng Cúc Phương
+    MaThienLanh = 1003, // Ma Thiên Lãnh 
+    NamCatTien = 1004, // Rừng Nam Cát Tiên
+
+    DongPhongNha = 2001, // Động Phong Nha
+    TamCocBichDong = 2002, // Tam Cốc Bích Động
+    DongHuongTich = 2003, // Động Hương Tích
+    DongThienCung = 2004, // Động Thiên Cung
+
+    NguHanhSon = 3001, // Ngũ Hành Sơn
+    CaoNguyenMocChau = 3002, // Cao Nguyên Mộc Châu
+    NuiTanVien = 3003, // Núi Tản Viên
+    NuiHamRong = 3004, // Núi Hàm Rồng
+
+    ThanhDiaMySon = 4001, // Thánh Địa Mỹ Sơn
+    ThanhCoLoa = 4002, // Thành Cổ Loa
+    VanMieu = 4003, // Văn Miếu
+
+    DamDaTrach = 5001, // Đầm Dạ Trạch
+    HoTaVong = 5002, // Hồ Tả Vọng
+
+    DongHaiLongCung = 6001, // Đông Hải Long Cung
 }
